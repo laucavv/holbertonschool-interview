337f1596510434d9ce5fa8a26e0c26fa5e85b4f6
laucavv <laucavv23@gmail.com>
2020-12-21 16:54:03 -0500

    fix betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node -  function that creates a binary tree node
+ * @parent: is a pointer to the parent node of the node to create
+ * @value: is the value to put in the new node
+ * Return: number of nodes
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
